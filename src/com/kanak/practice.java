5952f37e4454cf73b383603809e7431209644903
kanaksharma412 <kanaksharma412@gwu.edu>
2020-09-25 18:58:30 -0400

    removed a statement

--- src/com/kanak/practice.java
+++ src/com/kanak/practice.java
@@ -3,8 +3,8 @@ package com.kanak;
 public class practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("I am trying this out");
 	}
 
 }
